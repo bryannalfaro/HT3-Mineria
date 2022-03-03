2d024d1ee3f16b890d5023cfd920b93551e40823
Bryann Alfaro <alf19372@uvg.edu.gt>
2022-03-03 15:58:47 -0600

    feat: analisis exploratorio y comienzo de preprocesamiento

--- main.py
+++ main.py
@@ -29,4 +29,68 @@ import matplotlib.cm as cm
 
 houses = pd.read_csv('train.csv', encoding='latin1', engine='python')
 
+'''
+#Conocimiento de datos
 print(houses.head())
+
+#Cantidad de observaciones y variables en la base
+print(houses.shape)
+
+#Medidas estadisticas.
+print(houses.describe().transpose())
+
+print(houses.select_dtypes(exclude=['object']).info())'''
+
+
+'''#Casas que ofrecen todas las utilidades
+print(houses['Utilities'].value_counts())
+
+plt.bar(houses['Utilities'].value_counts().sort_index().dropna().index,houses['Utilities'].value_counts().sort_index().values,color='red')
+plt.title('Grafico de barras para utilidades')
+plt.xlabel('Utilidad')
+plt.xticks(rotation=90)
+plt.ylabel('Cantidad de casas')
+plt.tight_layout()
+plt.show()
+
+#Calidad de casas predominante
+print(houses['OverallCond'].value_counts())
+
+plt.bar(houses['OverallCond'].value_counts().sort_index().dropna().index,houses['OverallCond'].value_counts().sort_index().values,color='red')
+plt.title('Grafico de barras para condicion de las casas')
+plt.xlabel('Condicion')
+plt.ylabel('Cantidad de casas')
+plt.show()
+
+#Año de mas y menos produccion de casas para
+print(houses['YearBuilt'].value_counts().sort_values(ascending=False).head(1))
+print(houses['YearBuilt'].value_counts().sort_values(ascending=True).head(15))
+
+#Capacidad de carros de las 5 casas mas caras y baratas
+
+print(houses.sort_values(by='SalePrice', ascending=False)[['GarageCars','SalePrice']].head(5))
+print(houses.sort_values(by='SalePrice', ascending=True)[['GarageCars','SalePrice']].head(5))
+
+#Condicion de garage y calidad de la cocina de las 5 casas mas caras
+print(houses.sort_values(by='SalePrice', ascending=False)[['GarageCond','KitchenQual','SalePrice']].head(5))'''
+
+
+houses_clean = houses.select_dtypes(exclude='object').drop('Id', axis=1)
+'''#preprocesamiento
+corr_data = houses_clean.iloc[:,:]
+mat_correlation=corr_data.corr() # se calcula la matriz , usando el coeficiente de correlacion de Pearson
+plt.figure(figsize=(16,10))
+#Realizando una mejor visualizacion de la matriz
+sns.heatmap(mat_correlation,annot=True,cmap='BrBG')
+plt.title('Matriz de correlaciones  para la base Houses')
+plt.tight_layout()
+plt.show()'''
+
+houses_df = houses_clean[['OverallQual', 'GrLivArea', 'YearBuilt', 'YearRemodAdd', 'MasVnrArea', 'TotalBsmtSF', '1stFlrSF', 'FullBath', 'Fireplaces',
+'GarageCars', 'GarageArea', 'GarageYrBlt','TotRmsAbvGrd']]
+
+print(houses_df.head().dropna())
+print(houses_df.info())
+print(houses_df.describe().transpose())
+
+houses_df.fillna(0)
